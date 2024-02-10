de2bfb8d9e71da035a0854f0dc1d546285d36010
Matvei <matveiyan@yandex.ru>
2024-02-10 18:11:16 +0300

    add

--- Codeforces/104174-D.cpp
+++ Codeforces/104174-D.cpp
@@ -16,37 +16,28 @@ int dp[MAXN][2];
 int n, k;
 
 void dfs(int v) {
-    if (g[v].size() == 0) {
-        dp[v][0] = 1;
-        dp[v][1] = 0;
-        return;
-    }
-
     dp[v][0] = 1;
-    dp[v][1] = 1;
+    dp[v][1] = 0;
 
     for (auto u: g[v]) {
         dfs(u);
-        dp[v][0] = dp[v][0] * (dp[u][0] + dp[u][1]) % MOD;
+        dp[v][0] = (dp[v][0] * ((dp[u][0] + dp[u][1]) % MOD)) % MOD;
     }
 
-    if (k == 3) {
-        for (int i = 0; i < g[v].size(); ++i) {
-            for (int j = i + 1; j < g[v].size(); ++j) {
-                int u1 = g[v][i];
-                int u2 = g[v][j];
-                cout << u1 << ' ' << u2 << '\n';
+    for (int i = 0; i < g[v].size(); ++i) {
+        for (int j = i + 1; j < g[v].size(); ++j) {
+            int u1 = g[v][i];
+            int u2 = g[v][j];
 
-                int cur_dp = 1;
-                for (auto u: g[v]) {
-                    if (u == u1 || u == u2) {
-                        cur_dp = cur_dp * dp[u][0] % MOD;
-                    } else {
-                        cur_dp = cur_dp * (dp[u][0] + dp[u][1]) % MOD;
-                    }
+            int cur_dp = 1;
+            for (auto u: g[v]) {
+                if (u == u1 || u == u2) {
+                    cur_dp = cur_dp * dp[u][0] % MOD;
+                } else {
+                    cur_dp = (cur_dp * ((dp[u][0] + dp[u][1]) % MOD)) % MOD;
                 }
-                dp[v][1] = dp[v][1] * cur_dp % MOD;
             }
+            dp[v][1] = (dp[v][1] + cur_dp) % MOD;
         }
     }
 }
