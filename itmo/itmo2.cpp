9bf79d7aa1c8e23b79f474a27ecbf99ef522347f
Matvei <matveiyan@yandex.ru>
2024-02-25 19:50:22 +0300

    add

--- itmo/itmo2.cpp
+++ itmo/itmo2.cpp
@@ -1,74 +1,29 @@
-//  
+// 
 // 24.02.2024
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 #define int long long
 
+int func2(int x0, int x1, int x2, int x3) {
+    return (x0 || x1 && x2) && (x1 || x2 && x3) && (x2 || x3 && x0);
+}
+
+bitset<16> func(int N) {
+    bitset<64> b(N);
+    bitset<16> b2;
+    for (int i = 0; i < 64; i += 4) {
+        b2[i / 4] = func2(b[i], b[i + 1], b[i + 2], b[i + 3]);
+    }
+
+    return b2;
+}
+
 signed main() {
-    int tr[8][8] = {
-            {5, 5, 5, 5, 5, 5, 5, 5},
-            {5, 2, 3, 7, 4, 0, 1, 6},
-            {5, 3, 4, 1, 5, 3, 4, 1},
-            {5, 7, 1, 2, 4, 6, 3, 0},
-            {5, 4, 5, 4, 5, 4, 5, 4},
-            {5, 0, 3, 6, 4, 2, 1, 7},
-            {5, 1, 4, 3, 5, 1, 4, 3},
-            {5, 6, 1, 0, 4, 7, 3, 2},
-    };
-    int mas[8][8];
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            mas[i][j] = 0;
-        }
-    }
-
-    cout << "START\n";
-    for (int startN = 0; startN < 50000000; ++startN) {
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                mas[i][j] = 0;
-            }
-        }
-
-        int M = 8143989;
-
-        int N = startN;
-
-        while (M > 0) {
-            int i = 0;
-            while (i < 8) {
-                int j = 0;
-                while (j < 8) {
-                    if ((i * j) % 8 == M % 8) {
-                        mas[i][j] = N % 8;
-                    }
-                    ++j;
-                }
-                ++i;
-            }
-            N /= 8;
-            M /= 8;
-        }
-
-        bool flag = true;
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                if (tr[i][j] != mas[i][j]) {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if (flag) {
-            cout << "DONE: " << startN << '\n';
-            break;
-        }
-    }
-
+    cout << func(18364758544493064720).to_string();
 
     return 0;
 }
