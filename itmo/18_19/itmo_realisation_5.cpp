1e8f004ccecee956a9e80f6f49a63adce087a5d3
Matvei <matveiyan@yandex.ru>
2024-03-08 00:31:29 +0300

    add

---
+++ itmo/18_19/itmo_realisation_5.cpp
@@ -0,0 +1,68 @@
+// 
+// 08.03.2024
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+
+    vector<set<int>> obj(n);
+    for (int i = 0; i < n; ++i) {
+        int k;
+        cin >> k;
+        for (int j = 0; j < k; ++j) {
+            int prop;
+            cin >> prop;
+            obj[i].insert(prop);
+        }
+    }
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; ++i) {
+        char t;
+        int idx;
+        cin >> t >> idx;
+        if (t == '-') {
+            for (auto &o : obj) {
+                o.erase(idx);
+            }
+        } else {
+            --idx;
+            cout << obj[idx].size() << '\n';
+        }
+    }
+
+    return 0;
+}
+
+/*
+2
+3 1 2 4
+3 2 3 5
+12
+- 1
+? 1
+? 2
+- 2
+? 1
+? 2
+- 5
+? 1
+? 2
+- 6
+? 1
+? 2
+
+ */
