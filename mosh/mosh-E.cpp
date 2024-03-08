69fdb826f6d7fa2a9652cda0a9e468c87bf70a44
Matvei <matveiyan@yandex.ru>
2024-03-08 19:36:01 +0300

    add problems solutions

--- mosh/mosh-E.cpp
+++ mosh/mosh-E.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
@@ -12,24 +14,53 @@ signed main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    freopen("/mnt/d/Users/Matvei/Developer/Projects/SPP/mosh/tests/e2.txt", "r", stdin);
-    freopen("/mnt/d/Users/Matvei/Developer/Projects/SPP/mosh/output.txt", "w", stdout);
+    vector<pair<string, string>> codes;
+    ifstream in("/mnt/d/Users/Matvei/Developer/Projects/SPP/mosh/tests/e2.txt");
 
     int t;
-    cin >> t;
-
-    while (t--) {
+    in >> t;
+    for (int i = 0; i < t * 2; ++i) {
         string s1, s2;
-        cin >> s1 >> s2;
+        in >> s1 >> s2;
+        codes.emplace_back(s1, s2);
+    }
+    in.close();
 
-        char mx = 'a';
-        for (auto c: s1) {
-            mx = max(mx, c);
+    freopen("/mnt/d/Users/Matvei/Developer/Projects/SPP/mosh/mosh-E-solution.txt", "r", stdin);
+
+    int n = 10;
+
+    for (int idx = 0; idx < t; ++idx) {
+        map<string, char> table;
+        for (int i = 0; i < n; ++i) {
+            char c;
+            string s;
+            cin >> c >> s;
+            table[s] = c;
         }
 
-        cout << mx << ' ' << mx - 'a' << '\n';
-        cout << "len1: " << s1.size() << '\n';
-        cout << "len2: " << s2.size() << '\n';
+        string result;
+        string buffer;
+        for (char c : codes[idx].second) {
+            buffer += c;
+            int code_idx = -1;
+            for (int i = 0; i < n; ++i) {
+                if (table.contains(buffer)) {
+                    code_idx = i;
+                    break;
+                }
+            }
+            if (code_idx != -1) {
+                result += table[buffer];
+                buffer.clear();
+            }
+        }
+
+        if (result == codes[idx].first) {
+            cout << "TRUE\n";
+        } else {
+            cout << "FALSE\n";
+        }
     }
 
     return 0;
